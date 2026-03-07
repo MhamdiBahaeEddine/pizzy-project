70cf020ce4348e30819737456fffec43e4522145
Bahae Eddine Mhamdi <bahaeeddine120@gmail.com>
2026-03-07 12:47:49 +0000

    logo animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import { Home } from "./pages/Home";
+
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        React + Vite + Tailwind
-      </h1>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header"
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-black ">
+            <Header/>
+        </div>
+    )
+}
